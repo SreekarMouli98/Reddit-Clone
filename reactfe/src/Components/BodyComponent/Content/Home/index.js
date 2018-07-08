984051d6dc8f89eb0649288b8c0199664ce3257e
Mouli <sreekar.mouli1998@gmail.com>
2018-07-08 22:00:16 +0530

    reactfe - Added a temporary HomeComponent

--- reactfe/src/Components/BodyComponent/Content/Home/index.js
+++ reactfe/src/Components/BodyComponent/Content/Home/index.js
@@ -1,21 +1,31 @@
 import React, {Component} from 'react'
-import {Row, Col} from 'reactstrap'
+import Context from '../../../provider'
 import CardComponent from '../../OtherComponents/CardComponent'
+import {
+    Container,
+} from 'reactstrap'
 
 export default class Home extends Component {
     render() {
         return (
-            <React.Fragment>
-                <Row><Col sm="12"><h1>Home Content</h1></Col></Row>            
-                <Row><Col sm="12"><CardComponent /></Col></Row>
-                <Row><Col sm="12"><CardComponent /></Col></Row>
-                <Row><Col sm="12"><CardComponent /></Col></Row>
-                <Row><Col sm="12"><CardComponent /></Col></Row>
-                <Row><Col sm="12"><CardComponent /></Col></Row>
-                <Row><Col sm="12"><CardComponent /></Col></Row>
-                <Row><Col sm="12"><CardComponent /></Col></Row>
-                <Row><Col sm="12"><CardComponent /></Col></Row>
-            </React.Fragment>
+            <Context.Consumer>
+                {context => {
+                    return (
+                        <Container className='text-center'>
+                            <h1 className='display-1'>Welcome to Reddit</h1>
+                            {context.loggedIn ? 
+                                <React.Fragment>
+                                    <h1 className='display-4'>User logged in</h1>
+                                </React.Fragment>
+                                :
+                                <React.Fragment>
+                                    <h1 className='display-4'>Please Login</h1>
+                                </React.Fragment>                            
+                            }
+                        </Container>
+                    )
+                }}
+            </Context.Consumer>
         )
     }
 }
