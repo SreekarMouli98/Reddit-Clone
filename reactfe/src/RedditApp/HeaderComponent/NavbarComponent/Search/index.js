d94866bd10793501d926d21cb33dcb89b3307b1b
Mouli <sreekar.mouli1998@gmail.com>
2018-07-23 19:33:35 +0530

    fixed bug

--- reactfe/src/RedditApp/HeaderComponent/NavbarComponent/Search/index.js
+++ reactfe/src/RedditApp/HeaderComponent/NavbarComponent/Search/index.js
@@ -27,7 +27,7 @@ class Search extends Component {
         event.preventDefault()
         if (this.state.keyword.length !== 0) {
             document.getElementById('search-keyword').value = ''
-            this.props.history.push(`/search/${this.state.keyword}/`
+            this.props.history.push(`/search/${this.state.keyword}/`)
         }
     }
 
