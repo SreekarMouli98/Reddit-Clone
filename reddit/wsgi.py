b9a60c5d36c509f90eacb1e6ece10e83841048cb
Mouli <sreekar.mouli1998@gmail.com>
2018-08-06 18:36:51 +0530

    Fixed bug in wsgi

--- reddit/wsgi.py
+++ reddit/wsgi.py
@@ -11,6 +11,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reactdjango.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reddit.settings")
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
